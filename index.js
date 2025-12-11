9a266b394263fffc88e592f65fd373fa45253985
Akash Sarker <akashsarker2478@gmail.com>
2025-12-12 00:31:16 +0600

    payment system api add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const port =process.env.PORT|| 3000
 
 //middleware
@@ -26,18 +27,63 @@ async function run() {
 
 const db = client.db('digital_lesson_db')
 const lessonsCollection = db.collection('lessons');
+const usersCollection = db.collection('users');
+
+
+//user API
+// app.post('/users',async(req,res)=>{
+//   const user = req.body;
+//   const  query = {email:user.email}
+//   const existingUser = await usersCollection.findOne(query);
+//   if (existingUser) {
+       
+//         return res.send({ message: 'User already exists', insertedId: existingUser._id });
+//     }
+//     const newUser = {
+//       ...user,
+//       isPremium:false,
+//       joinDate: new Date(),
+//     }
+//     const result = await usersCollection.insertOne(newUser);
+//     res.send(result);
+// })
+
+
+// app.get('/user-plan/:email', async (req, res) => {
+//     const email = req.params.email;
+//     const query = { email: email };
+
+//     const user = await usersCollection.findOne(query);
+
+//     if (!user) {
+       
+//         return res.send({ isPremium: false }); 
+//     }
+
+   
+//     res.send({ isPremium: user.isPremium });
+// });
+
 
 //lessons api
-app.get('/lessons',async(req,res)=>{
-const query = {}
-const email = req.query.email;
-if(email){
-    query.createdBy = email;
-}
-const cursor = lessonsCollection.find(query)
-const result = await cursor.toArray()
-res.send(result)
-})
+app.get('/lessons', async (req, res) => {
+  try {
+    const query = {};
+    const email = req.query.email;
+
+    if (email) {
+      query.createdBy = email;
+    }
+
+    const result = await lessonsCollection.find(query).sort({ createdAt: -1 }).toArray();
+
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Failed to fetch lessons' });
+  }
+});
+
 
 app.post('/lessons',async(req,res)=>{
     const lesson = req.body;
@@ -52,6 +98,39 @@ app.delete('/lessons/:id',async(req,res)=>{
   const query = {_id: new ObjectId(id)}
   const result = await lessonsCollection.deleteOne(query)
   res.send(result)
+})
+
+//payment related apis
+app.post('/create-checkout-session',async(req,res)=>{
+  const {userEmail,userId} = req.body;
+  const AMOUNT_BDT = 1500;
+  const UNIT_AMOUNT = AMOUNT_BDT * 100;
+   const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price_data:{
+          currency:'BDT',
+          unit_amount:UNIT_AMOUNT,
+           product_data: {
+              name: "Lifetime Premium Access",
+              description: "Get unlimited premium lessons forever."
+            },
+        },
+        
+        quantity: 1,
+      },
+    ],
+    customer_email:userEmail,
+    mode: 'payment',
+    metadata:{
+      userId:userId
+    },
+    success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+  });
+console.log(session)
+res.send({url:session.url})
 })
 
     // Send a ping to confirm a successful connection
